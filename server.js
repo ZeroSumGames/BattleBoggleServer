0766e198014720bd02021d023a98ae09e737acf8
jasenvchan <jachan@gwu.edu>
2020-01-27 19:07:45 -0500

    fixed process env PORT

--- server.js
+++ server.js
@@ -52,8 +52,8 @@ function makeBoard(letters) {
 let board = makeBoard(allLetters);
 
 
-server.listen(process.env.port || 3000, () => {
-	if(process.env.port) console.log(`Serving on port ${process.env.port}`)
+server.listen(process.env.PORT || 3000, () => {
+	if(process.env.PORT) console.log(`Serving on port ${process.env.PORT}`)
 	else console.log("Couldn't find process.env.port...started serving on port 3000")
 })
 
